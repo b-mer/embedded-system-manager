daa382682809431f733fb6cfaae52b3cc4a1b4ba
b-mer <163482111+b-mer@users.noreply.github.com>
2025-10-03 09:18:51 +1000

    Fell back to using a info box instead of gauge

--- embedded-system-manager/config_setup.sh
+++ embedded-system-manager/config_setup.sh
@@ -32,27 +32,10 @@ configuration_setup() {
 			echo "Configuration setup cancelled."
 			exit 1
 		fi
-		# Run git command in background and show progress
-		tmpfile=$(mktemp)
-		(GIT_TERMINAL_PROMPT=0 git ls-remote --exit-code "$GIT_REPO" >/dev/null 2>&1; echo $? > "$tmpfile") &
-		git_pid=$!
-		{
-			for i in 0 10 20 30 40 50 60 70 80 90; do
-				if kill -0 $git_pid 2>/dev/null; then
-					echo $i
-					sleep 0.2
-				else
-					break
-				fi
-			done
-			while kill -0 $git_pid 2>/dev/null; do
-				sleep 0.1
-			done
-			echo 100
-			sleep 0.2
-		} | whiptail --gauge "Checking if repository can be accessed..." 6 50 0 < /dev/tty
-		exit_code=$(cat "$tmpfile")
-		rm -f "$tmpfile"
+		# Run git command and show progress with infobox
+		whiptail --infobox "Checking if repository can be accessed...\n\nPlease wait..." 8 50 < /dev/tty
+		GIT_TERMINAL_PROMPT=0 git ls-remote --exit-code "$GIT_REPO" >/dev/null 2>&1
+		exit_code=$?
 		if [ "$exit_code" -eq 0 ]; then
 			break
 		else
@@ -66,27 +49,10 @@ configuration_setup() {
 			echo "Configuration setup cancelled."
 			exit 1
 		fi
-		# Run git command in background and show progress
-		tmpfile=$(mktemp)
-		(GIT_TERMINAL_PROMPT=0 git ls-remote --exit-code "$GIT_REPO" "$GIT_REPO_BRANCH" >/dev/null 2>&1; echo $? > "$tmpfile") &
-		git_pid=$!
-		{
-			for i in 0 10 20 30 40 50 60 70 80 90; do
-				if kill -0 $git_pid 2>/dev/null; then
-					echo $i
-					sleep 0.2
-				else
-					break
-				fi
-			done
-			while kill -0 $git_pid 2>/dev/null; do
-				sleep 0.1
-			done
-			echo 100
-			sleep 0.2
-		} | whiptail --gauge "Checking if branch can be accessed..." 6 50 0 < /dev/tty
-		exit_code=$(cat "$tmpfile")
-		rm -f "$tmpfile"
+		# Run git command and show progress with infobox
+		whiptail --infobox "Checking if branch can be accessed...\n\nPlease wait..." 8 50 < /dev/tty
+		GIT_TERMINAL_PROMPT=0 git ls-remote --exit-code "$GIT_REPO" "$GIT_REPO_BRANCH" >/dev/null 2>&1
+		exit_code=$?
 		if [ "$exit_code" -eq 0 ] || [ -z "$GIT_REPO_BRANCH" ]; then
 			break
 		else
