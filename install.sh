698bd094f9aaa2cf36ac0fd52b4e9b4b00f52197
Briar Merrett <163482111+b-mer@users.noreply.github.com>
2025-10-03 07:12:17 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+# Check if whiptail is installed.
+if ! command -v whiptail &> /dev/null; then
+  echo "Error: whiptail is not installed." >&2
+  exit 1
+fi
+# Check if git is installed.
+if ! command -v git &> /dev/null; then
+  echo "Error: git is not installed." >&2
+  exit 1
+fi
+
+echo "Cloning repository into /tmp/embedded-system-manager..."
+rm -rf /tmp/embedded-system-manager   
+git clone https://github.com/b-mer/embedded-system-manager.git /tmp/embedded-system-manager
+
+chmod +x /tmp/embedded-system-manager/setup.sh
+. /tmp/embedded-system-manager/setup.sh
