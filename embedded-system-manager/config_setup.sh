96122220fa46fc8319e2f1d9e1f2058c79f912cc
Briar Merrett <163482111+b-mer@users.noreply.github.com>
2025-10-02 22:48:39 +0000

    Attempted fix at fixing the gauge in config setup

--- embedded-system-manager/config_setup.sh
+++ embedded-system-manager/config_setup.sh
@@ -28,12 +28,11 @@ configuration_setup() {
 	# Get repository
 	while true; do
 		GIT_REPO=$(whiptail --inputbox "Git repository clone link:" 8 80 --title "$SETUP_TITLE" 3>&1 1>&2 2>&3  < /dev/tty)
-		GIT_TERMINAL_PROMPT=0 git ls-remote --exit-code "$GIT_REPO" >/dev/null 2>&1
-		sleep 2
 		if [ "$?" = 1 ]; then
 			echo "Configuration setup cancelled."
 			exit 1
 		fi
+		stdbuf -oL \
 		{
 			echo "33"
 			GIT_TERMINAL_PROMPT=0 git ls-remote --exit-code "$GIT_REPO" >/dev/null 2>&1
@@ -54,6 +53,7 @@ configuration_setup() {
 			echo "Configuration setup cancelled."
 			exit 1
 		fi
+		stdbuf -oL \
 		{
 			echo "33"
 			GIT_TERMINAL_PROMPT=0 git ls-remote --exit-code "$GIT_REPO" "$GIT_REPO_BRANCH" >/dev/null 2>&1
